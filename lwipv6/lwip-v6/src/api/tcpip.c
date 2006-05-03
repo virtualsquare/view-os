cccef4d7023826e658379cbfbb0a45bf532c8544
Diego Billi <diegobilli@gmail.com>
2006-05-03 21:54:00 +0000

    Removed a debug printf() in tcpip.c

--- lwipv6/lwip-v6/src/api/tcpip.c
+++ lwipv6/lwip-v6/src/api/tcpip.c
@@ -167,7 +167,6 @@ tcpip_thread(void *arg)
 	while (loop) {                          /* MAIN Loop */
 
 		LWIP_DEBUGF(TCPIP_DEBUG, ("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n"));
-		printf ("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
 
 		LWIP_DEBUGF(TCPIP_DEBUG, ("tcpip_thread: waiting4message\n"));
 		sys_mbox_fetch(mbox, (void *)&msg);
@@ -219,7 +218,6 @@ tcpip_thread(void *arg)
 		}
 
 		LWIP_DEBUGF(TCPIP_DEBUG, ("----------------------------------------------------------------------------\n"));
-		printf("----------------------------------------------------------------------------\n");
 	}
 
 	// FIX: this is not enough, after this call netif threads are still alive
