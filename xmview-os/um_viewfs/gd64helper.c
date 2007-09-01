292d4bec667ab92375d880e374b49e5d67585762
Ludovico Gardenghi <lu@dovi.co>
2007-09-01 22:52:40 +0000

    work in progress on getdents64 helper/caching mechanism

---
+++ xmview-os/um_viewfs/gd64helper.c
@@ -0,0 +1,122 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <glib.h>
+#include <string.h>
+#include <linux/dirent.h>
+#include <linux/unistd.h>
+#include "gdebug.h"
+
+struct _dirdata {
+	long pos;
+	long size;
+	
+	/* dents is a flat "array" with variable sized elements */
+	struct dirent64 *dents;
+	struct dirent64 *last;
+
+	/* tree is a tree of telem which contains a pointer to a dirent
+	 * in dents */
+	GTree *tree;
+};
+
+struct _telem {
+	struct dirent64 *cur;
+	struct dirent64 *prev;
+	struct dirent64 *next;
+	unsigned char skip;
+};
+
+typedef struct _dirdata dirdata;
+typedef struct _telem telem;
+
+
+dirdata *dirdata_new()
+{
+	dirdata *new = malloc(sizeof(dirdata));
+
+	new->dents = NULL;
+	new->last = NULL;
+	new->size = 0;
+	new->tree = g_tree_new((GCompareFunc)*strcmp);
+	new->pos = 0;
+
+	return new;
+}
+
+void dirdata_add_dirent(dirdata *dd, struct dirent64 *dent)
+{
+	telem *tnew;
+	struct dirent64 *dnew;
+
+	dd->size += dent->d_reclen;
+	dd->dents = realloc(dd->dents, dd->size);
+
+	if (dd->size == dent->d_reclen) // First element
+		dnew = dd->dents;
+	else
+		dnew = dd->last->d_off;
+
+	memcpy(dnew, dent, dd->size);
+
+	tnew = malloc(sizeof(tnew));
+	tnew->cur = dnew;
+	tnew->next = NULL;
+
+	if (dd->size == dent->d_reclen) // First element
+	{
+		tnew->prev = NULL;
+		dnew->d_off = dnew->d_reclen;
+	}
+	else
+	{
+		tnew->prev = dd->last;
+		dnew->d_off = 
+		dd->end->d_off = dd->last->d_off + dd->end->d_reclen;
+		tnew->prev = dd->last;
+	}
+
+	tnew->skip = 0;
+
+	dd->last = dd->end;
+	dd->end += dd->size;
+
+	g_tree_insert(dd->tree, (dd->last).d_name, dd->last);
+}
+
+void dirdata_add_dirents(dirdata *dd, struct dirent64 *dents, unsigned int count)
+{
+	int pos = 0;
+
+	while (pos < count)
+	{
+		dirdata_add_dirent(dd, dents);
+		pos += dents->d_reclen;
+	}
+}
+
+int dirdata_remove_dirent(dirdata *dd, char *name)
+{
+	telem *result;
+
+	result = g_tree_lookup(dd->tree, name);
+
+	if (!result)
+		return 0;
+
+	result->skip = 1;
+	result->prev->d_off += result->
+
+	g_tree_remove(dd->tree, name);
+
+
+
+
+
+}
+
+void dirdata_free(dirdata *dd)
+{
+	g_tree_destroy(dd->tree);
+	free(dd->dents);
+}
+
