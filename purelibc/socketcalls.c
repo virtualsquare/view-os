bd867a3a90b0a2de03f0b93b337e167620556124
Filippo Giunchedi <fgiunchedi@gmail.com>
2008-02-06 22:48:35 +0000

    exclude some more archs from socketcalls

--- purelibc/socketcalls.c
+++ purelibc/socketcalls.c
@@ -32,7 +32,7 @@ static long int pure_int_socketcall(long int, ...);
 sfun _pure_socketcall=pure_int_socketcall;
 static char sockargc[]={0,3,3,3,2,3,3,3,4,4,4,6,6,2,5,5,3,3};
 
-#if defined(__x86_64__)
+#if defined(__x86_64__) || defined(__ia64__) || defined(__alpha__) || defined(__hppa__)
 
 static struct socket64_mapping{
 	int socketcallno;
@@ -111,14 +111,14 @@ int socketpair(int d, int type, int protocol, int sv[2]){
 	return _pure_socketcall(SYS_SOCKETPAIR,d,type,protocol,sv);
 }
 ssize_t send(int s, const void *buf, size_t len, int flags){
-#if defined(__x86_64__)
+#if defined(__x86_64__) || defined(__ia64__) || defined(__alpha__) || defined(__hppa__)
 	return sendto(s,buf,len,flags,NULL,0);
 #else
 	return _pure_socketcall(SYS_SEND,s,buf,len,flags);
 #endif
 }
 ssize_t recv(int s, void *buf, size_t len, int flags){
-#if defined(__x86_64__)
+#if defined(__x86_64__) || defined(__ia64__) || defined(__alpha__) || defined(__hppa__)
 	return recvfrom(s,buf,len,flags,NULL,0);
 #else
 	return _pure_socketcall(SYS_RECV,s,buf,len,flags);
