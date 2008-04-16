84ffff1d254b2fe4e8eefe73ee312f345efbc63e
Ludovico Gardenghi <lu@dovi.co>
2008-04-16 20:02:33 +0000

    Tagged purelibc-0.4

---
+++ 0.4/purelibc.h
@@ -0,0 +1,13 @@
+#ifndef _PURELIBC_H
+#define _PURELIBC_H
+
+typedef long int (*sfun)(long int __sysno, ...);
+
+#define PUREFLAG_STDIN (1<<STDIN_FILENO)
+#define PUREFLAG_STDOUT (1<<STDOUT_FILENO)
+#define PUREFLAG_STDERR (1<<STDERR_FILENO)
+#define PUREFLAG_STDALL (PUREFLAG_STDIN|PUREFLAG_STDOUT|PUREFLAG_STDERR)
+
+sfun _pure_start(sfun pure_syscall,sfun pure_socketcall,int flags);
+
+#endif
