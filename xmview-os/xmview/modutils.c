2c86895f8f8563f05fe27f4230055aa70893b4ef
Ludovico Gardenghi <lu@dovi.co>
2008-06-18 10:38:27 +0000

    * Added ~/.umview/modules to modules search path

--- xmview-os/xmview/modutils.c
+++ xmview-os/xmview/modutils.c
@@ -27,24 +27,45 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <config.h>
 #include "gdebug.h"
 
+/* This will be prefixed with getent("$HOME") */
+#define USER_MODULES_DIR "/.umview/modules"
+
+#ifndef MAX
+#	define MAX(a, b) ((a) > (b) ? (a) : (b))
+#endif
 /*
  * Try to dlopen a module (o submodule) trying different names and locations:
  * 
  * 1) dlopen(modname)
  * 2) dlopen(modname.so)
- * 3) dlopen(umview_plugin_directory/modname)
- * 4) dlopen(umview_plugin_directory/modname.so)
+ * 3) dlopen(user_umview_plugin_directory/modname)
+ * 4) dlopen(user_umview_plugin_directory/modname.so)
+ * 5) dlopen(global_umview_plugin_directory/modname)
+ * 6) dlopen(global_umview_plugin_directory/modname.so)
  *
  */
+
+#define TRY_DLOPEN(fmt...) \
+{ \
+	snprintf(testpath, tplen, fmt); \
+	fprintf(stderr, "trying %s\n", testpath); \
+	if ((handle = dlopen(testpath, flag))) \
+	{ \
+		free(testpath); \
+		return handle; \
+	} \
+}
 
 void *openmodule(const char *modname, int flag)
 {
 	void *handle;
 	char *testpath;
 	int tplen;
+	char *homedir = getenv("HOME");
 
 	if (!modname)
 		return NULL;
@@ -52,23 +73,22 @@ void *openmodule(const char *modname, int flag)
 	if ((handle = dlopen(modname, flag)))
 		return handle;
 
+	/* If there is no home directory, use CWD */
+	if (!homedir)
+		homedir = ".";
+
 	tplen = strlen(modname) +
-		strlen(MODULES_EXT) +
-		strlen(MODULES_DIR) + 2; // + 1 is for a '/' and + 1 for \0
+		strlen(MODULES_EXT) + 2 + // + 1 is for a '/' and + 1 for \0
+		MAX(strlen(MODULES_DIR),
+		    strlen(homedir) + strlen(USER_MODULES_DIR));
 
-	testpath = alloca(tplen);
+	testpath = malloc(tplen);
 
-	snprintf(testpath, tplen, "%s%s", modname, MODULES_EXT);
-	if ((handle = dlopen(testpath, flag)))
-		return handle;
-
-	snprintf(testpath, tplen, "%s/%s", MODULES_DIR, modname);
-	if ((handle = dlopen(testpath, flag)))
-		return handle;
-	
-	snprintf(testpath, tplen, "%s/%s%s", MODULES_DIR, modname, MODULES_EXT);
-	if ((handle = dlopen(testpath, flag)))
-		return handle;
+	TRY_DLOPEN("%s%s", modname, MODULES_EXT);
+	TRY_DLOPEN("%s%s/%s", homedir, USER_MODULES_DIR, modname);
+	TRY_DLOPEN("%s%s/%s%s", homedir, USER_MODULES_DIR, modname, MODULES_EXT);
+	TRY_DLOPEN("%s%s", MODULES_DIR, modname);
+	TRY_DLOPEN("%s/%s%s", MODULES_DIR, modname, MODULES_EXT);
 	
 	return NULL;
 }
